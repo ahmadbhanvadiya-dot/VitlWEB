297efb3a1f61e0eb953633cec054f40f81c27a8f
ahmadbhanvadiya-dot <ahmadbhanvadiya@gmail.com>
2026-06-14 08:12:45 +0530

    fix supabase crash in emergency route

--- app/e/[id]/page.tsx
+++ app/e/[id]/page.tsx
@@ -1,5 +1,5 @@
 import EmergencyClient from "../../EmergencyClient";
-import { supabase } from "../../../lib/supabase";
+import { supabase } from "@/lib/supabase";
 import { notFound } from "next/navigation";
 
 export default async function EmergencyPage({

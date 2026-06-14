798236eb4afd2e778dd912f7357d059652e12957
ahmadbhanvadiya-dot <ahmadbhanvadiya@gmail.com>
2026-06-14 09:34:50 +0530

    test

--- app/e/[id]/page.tsx
+++ app/e/[id]/page.tsx
@@ -1,29 +1,9 @@
-import EmergencyClient from "../../EmergencyClient";
-import { supabase } from "@/lib/supabase";
-import { notFound } from "next/navigation";
+export default async function EmergencyPage(props: any) {
+  console.log("FULL PROPS:", JSON.stringify(props, null, 2));
 
-export default async function EmergencyPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const id = params.id;
-
-  console.log("🔥 PAGE HIT:", id);
-
-  const { data, error } = await supabase
-  .from("emergency_profiles")
-  .select("*")
-  .eq("emergency_id", params.id)
-  .maybeSingle();
-
-console.log("PARAM ID:", params.id);
-console.log("SUPABASE DATA:", data);
-console.log("SUPABASE ERROR:", error);
-
-  if (!data) {
-    return <div>Emergency profile not found</div>;
-  }
-
-  return <EmergencyClient data={data} />;
+  return (
+    <pre>
+      {JSON.stringify(props, null, 2)}
+    </pre>
+  );
 }
